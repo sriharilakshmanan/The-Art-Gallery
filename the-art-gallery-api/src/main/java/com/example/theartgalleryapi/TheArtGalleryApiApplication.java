9c75597bfe5802f9c008b90c5b7d5af7314b3aab
Sri Hari Lakshmanan <sri-hari.lakshmanan@socgen.com>
2020-04-12 22:53:01 +0530

    Basic UI and Spring Boot Starter -  First commit

---
+++ the-art-gallery-api/src/main/java/com/example/theartgalleryapi/TheArtGalleryApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.theartgalleryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheArtGalleryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheArtGalleryApiApplication.class, args);
+	}
+
+}
---
+++ the-art-gallery-api/src/test/java/com/example/theartgalleryapi/TheArtGalleryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.theartgalleryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheArtGalleryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
